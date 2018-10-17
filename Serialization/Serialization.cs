02acb22e262dc4e33524be7848de6cf71edfb90e
Eduard <edikzaxarin@gmail.com>
2018-10-17 10:58:34 +0300

    Added serialization class

---
+++ Serialization/Serialization.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using LogicClass;
+using ClassLibrary;
+using System.ComponentModel;
+using System.Collections.ObjectModel;
+using System.Xml.Serialization;
+using System.IO;
+
+namespace Serialization
+{
+    public class Serialization
+    {
+        public void saveShapes(string path,ObservableCollection<PolygonShape> polygonShapes)
+        {
+            XmlSerializer xmlSerializer = new XmlSerializer(polygonShapes.GetType(),new Type[] { typeof(PolygonShape)});
+            using (FileStream fs = new FileStream(path, FileMode.OpenOrCreate, FileAccess.ReadWrite))
+            {
+                xmlSerializer.Serialize(fs, polygonShapes);
+            }
+        }
+        public ObservableCollection<PolygonShape> openShapes(string path)
+        {
+            ObservableCollection<PolygonShape> polygons = new ObservableCollection<PolygonShape>();
+            XmlSerializer xmlSerializer = new XmlSerializer(typeof(ObservableCollection<PolygonShape>),new Type[] { typeof(PolygonShape)});
+
+            using (FileStream fs = new FileStream(path, FileMode.Open, FileAccess.ReadWrite))
+            {
+                polygons = (ObservableCollection<PolygonShape>)xmlSerializer.Deserialize(fs);
+            }
+            return polygons;
+        }
+    }
+}
